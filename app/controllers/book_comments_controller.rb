b6b7874e88f785c429e4a55fb14567447f04c17f
vagrant <vagrant@localhost.localdomain>
2020-06-06 12:20:08 +0000

    [Update]CommentAjax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,16 +2,16 @@ class BookCommentsController < ApplicationController
   before_action :correct_user, only: [:destroy]
 
   def create
-    @book_c = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = @book_c.id
-    @comment.save
-    # redirect_to book_path(@book_c)
+    @book_show = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book_show.id
+    @book_comment.save
+    # redirect_to book_path(@book_show)
   end
 
   def destroy
-    @comment = BookComment.find(params[:book_id])
-    @comment.destroy
+    @book_comment = BookComment.find(params[:id])
+    @book_comment.destroy
     # redirect_to book_path(@comment.book)
   end
 

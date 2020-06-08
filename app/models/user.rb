0193195bfeb6ee73f87ac1814a3df033d2703d98
vagrant <vagrant@localhost.localdomain>
2020-06-08 07:44:30 +0000

    [Update]devise:views

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,13 +57,13 @@ class User < ApplicationRecord
 
   #prefecture_codeからprefecture_nameに変換するメソッド
   include JpPrefecture
-  jp_prefecture :prefecture_code
+ jp_prefecture :prefecture_code
 
-  def prefecture_name
-    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
-  end
+ def prefecture_name
+     JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+ end
 
-  def prefecture_name=(prefecture_name)
-    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
-  end
+ def prefecture_name=(prefecture_name)
+     self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+ end
 end
